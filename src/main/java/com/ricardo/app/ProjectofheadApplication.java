88abcd5ae4d09a00500b8607b0866362bef48ad4
RicScomputer <3476952607@qq.com>
2022-05-08 15:14:27 +0800

    lizhenghao

---
+++ src/main/java/com/ricardo/app/ProjectofheadApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectofheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectofheadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/app/ProjectofheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectofheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
